121483148dbef8434be2a4aa1a5ebf73285da770
GarciaJared <gjared388@gmail.com>
2024-09-01 16:44:41 -0600

    [ADD] Brief section

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'REDGIR';
-export const SITE_DESCRIPTION = 'Habla tu idioma favorito en @ClasesRedgir';
+export const SITE_DESCRIPTION = 'Habla tu idioma favorito en @IdiomasRedgir';
