00b957de5eb29b03df8860914607aa95fa03277a
GarciaJared <gjared388@gmail.com>
2024-11-24 21:50:10 -0600

    [FIX] responsive

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'REDGIR';
-export const SITE_DESCRIPTION = 'Habla tu idioma favorito en @IdiomasRedgir';
+export const SITE_TITLE = 'Clases de idiomas REDGIR';
+export const SITE_DESCRIPTION = 'Habla tu idioma favorito en @redgir.idiomas';
