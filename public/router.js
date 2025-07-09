0788207633aaf9406cc04196f3f8faa088a3142e
Jared García <desarrollo.web@grupogardea.com>
2025-07-09 16:07:34 -0600

    Add basic vanilla router

---
+++ public/router.js
@@ -0,0 +1,42 @@
+class Router {
+    constructor(routes) {
+        this.routes = routes;
+        this._loadInitialRoute();
+        window.addEventListener('popstate', () => {
+            const pathNameSplit = window.location.pathname.split('/');
+            const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : [''];
+            this.loadRoute(...pathSegs);
+        });
+    }
+
+    loadRoute(...urlSegs) {
+        const matchedRoute = this._matchUrlToRoute(urlSegs);
+
+        const url = `/${urlSegs.join('/')}`;
+        history.pushState({}, '', url);
+
+        const routerOutElm = document.querySelector('[data-router]');
+        if (routerOutElm && matchedRoute) {
+            routerOutElm.innerHTML = matchedRoute.template;
+        }
+    }
+
+    _matchUrlToRoute(urlSegs) {
+        const matchedRoute = this.routes.find(route => {
+            const routePathSegs = route.path.split('/').slice(1);
+            if (routePathSegs.length !== urlSegs.length) {
+                return false;
+            }
+            return routePathSegs.every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+        });
+        return matchedRoute;
+    }
+
+    _loadInitialRoute() {
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : [''];
+        this.loadRoute(...pathSegs);
+    }
+}
+
+export default Router;
